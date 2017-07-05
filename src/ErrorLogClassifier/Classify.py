139d0f310e7c08c1c5d3c2ae23601677187ad60c
Mateo Torres Ruiz <mateo.atr@gmail.com>
2017-07-05 11:09:42 -0700

    Add ability to classify batches of logs, add class to each category, ignore .pickle files.

--- src/ErrorLogClassifier/Classify.py
+++ src/ErrorLogClassifier/Classify.py
@@ -1,35 +1,33 @@
-import jellyfish
 import json
 import numpy as np
 import pandas as pd
 import pickle
 import sys
 
-from pprint import pprint
-from difflib import SequenceMatcher
+from LogReport import LogReport
 from sklearn import svm, preprocessing
 
 class Classify:
 
-    '''
-    Function used by the word-distance based model.
-    
-    def __custom_distance(self, i,j):
-        return (SequenceMatcher(None, i, j).ratio() * jellyfish.jaro_winkler(unicode(i),unicode(j)) * 1000) * (10000 - jellyfish.damerau_levenshtein_distance(unicode(i),unicode(j)))
-    '''
-
-    def classifyLog(self, log):
-        log = log.lower()
-        vector = [0] * (len(self.keywords))
-        for word in range(0, len(self.keywords)):
-            if self.keywords[word] in log:
-                vector[word] = vector[word] + 1
-        vector = np.array(vector)
-
+    def classifyLogs(self, logs):
         with open('clusters.json') as f:
             data = json.load(f)
+        logReports = []
 
-        return json.dumps(data["clusters"][int(self.linearClf.predict(vector.reshape(1,-1))[0])])
+        for log in logs:
+            original = log
+            log = log.lower()
+            vector = [0] * (len(self.keywords))
+            for word in range(0, len(self.keywords)):
+                if self.keywords[word] in log:
+                    vector[word] = vector[word] + 1
+            vector = np.array(vector)
+
+            categoryAndClass = json.dumps(data["clusters"][int(self.linearClf.predict(vector.reshape(1,-1))[0])])
+            categoryAndClass = json.loads(categoryAndClass)
+            logReports.append(LogReport(original, categoryAndClass["category"], categoryAndClass["class"]))
+
+        return json.dumps([LogReport.returnJson(logReport) for logReport in logReports])
 
     def __init__(self):
         self.keywords = pd.read_csv("keywords.csv")['Keyword'].tolist()
